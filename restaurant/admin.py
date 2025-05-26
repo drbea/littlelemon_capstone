74bae5dc4e72f1fe83f11d5befff436b82a43484
drbea <beavoguimaximeakoi@gmail.com>
2025-05-26 16:18:50 +0000

    setting up database using mysql with django and configuring db model , adding two models menu and booking class

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Booking, Menu
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+	pass
